3089a27b0affd06404774cee7951e36bca632faa
Rafael Estrada <restradap1600@gmail.com>
2020-08-30 23:38:19 -0500

    Se agregaron aplicaciones a bucles.py

--- bucles.py
+++ bucles.py
@@ -16,3 +16,25 @@ if asignatura in asignaturas:
     print("Se ha seleccionado correctamente:", asignatura)
 else:
     print("No se encontro la siguiente asignatura:", asignatura)
+
+# Imprime numeros en una sola linea
+print("Aplicacion del conteo")
+for numero in range(0, 10):
+    print(numero, end="")
+
+# Uso de break, continue y else
+print("Aplicacion de operaciones en bucles")
+for nombre in ["Pedro", "Rafael", "Juan", "Pepe"]:
+    if nombre == "Rafael":
+        print("Ey, compartimos nombre!")
+        continue
+    elif nombre != "Juan":
+        print("Hola," + nombre)
+    else:
+        print("El nombre es juan")
+        # break ya no tomará en cuenta a la sentencia else
+        break
+
+    print("Iteracion")
+else:
+    print("Hola Mundo")
